55c42b7f3ead3a3f6a10f9cca7d7801b41283c7b
Exotic9ine <59295323+Exotic9ine@users.noreply.github.com>
2020-11-11 21:40:10 +0200

    Adding Parse and Terminal functions

--- Command Line Interpreter/src/Terminal.java
+++ Command Line Interpreter/src/Terminal.java
@@ -1,4 +1,9 @@
 
-public class Terminal {
-
+public class Terminal{ 
+//	public void cp(String sourcePath, String destinationPath ); 
+//	public void mv(String sourcePath, String destinationPath); 
+//	public void rm(String sourcePath); 
+//	public void pwd(); 
+//	public void cat(String[] paths); // Add any other required command in the same structure…..
+//	// Main implementation is up to you
 }
