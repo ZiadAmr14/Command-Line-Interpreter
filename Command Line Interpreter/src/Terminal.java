189bd4e28bbae4eba7702f41b554e039583a05a1
Ibrahim563 <64958351+Ibrahim563@users.noreply.github.com>
2020-11-12 13:14:16 +0200

    commented new functions

--- Command Line Interpreter/src/Terminal.java
+++ Command Line Interpreter/src/Terminal.java
@@ -3,7 +3,10 @@ public class Terminal{
 //	public void cp(String sourcePath, String destinationPath ); 
 //	public void mv(String sourcePath, String destinationPath); 
 //	public void rm(String sourcePath); 
-//	public void pwd(); 
-//	public void cat(String[] paths); // Add any other required command in the same structure…..
+//	public void pwd(); //display current user directory
+//	public void cat(String[] paths);  // print the file content & concatenate with other file(s) contents 
+//  public void rmdir(String path); //remove directory
+//	public void pipeOperator(function1,function2)// takes the output of the first command as a input for the second command
+	
 //	// Main implementation is up to you
 }
